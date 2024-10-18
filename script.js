2e76d9514e236b2d055a6bc66301440bc0dc03e9
katherine011 <guluaq09@gmail.com>
2024-10-18 06:05:55 +0400

    homework.js01

---
+++ script.js
@@ -0,0 +1,42 @@
+let Name = "keti"
+console.log(Name)
+
+let LastName = "Gulua"
+console.log(LastName)
+
+let Age = 19
+console.log(Age)
+
+let AgeChecked = true
+console.info("%cadult", "color: yellow")
+
+let Email = "guluaq09@gmail.com"
+console.log(Email)
+
+let Number = 595910507 
+console.info("%c595910507", "color:red")
+
+
+
+
+
+
+
+
+
+
+
+
+// let AboutMe = {
+//     Name : "keti",
+//     LastName : "Gulua",
+//     Age : 19,
+//     Email : "guluaq09@gmailcom",
+//     Number : 595910507, 
+//     AgeChecked : true
+// }
+
+// console.log(AboutMe)
+
+// console.info("%c595910507", "background-color: red")
+// console.info("%cAgeChecked : true", "background-color: yellow")
